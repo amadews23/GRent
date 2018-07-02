542c072256d58291fd6211f7037929b6061196a9
Bartolomé Vich Lozano <tolo@tovilo.es>
2018-07-02 14:14:07 +0200

    Add files via upload

---
+++ doc/rent.sql
@@ -0,0 +1,58 @@
+CREATE TABLE "GENERO" (
+    "IdGenero" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "nombre" TEXT
+);
+CREATE TABLE VIDEOJUEGO(
+ IdVideojuego    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+  nombre  TEXT,
+  descripcion TEXT,
+  IdGenero INTEGER,
+  precioalquiler FLOAT,
+  FOREIGN KEY (IdGenero) REFERENCES genero(IdGenero) ON UPDATE  CASCADE ON DELETE SET NULL
+);
+CREATE TABLE PROVEEDOR (
+    "IdProveedor" INTEGER NOT NULL,
+    "nombre" TEXT,
+    "domicilio" TEXT,
+    "telefono1" INTEGER,
+    "telefono2" INTEGER,
+    "email" TEXT,
+    "observaciones" TEXT
+);
+CREATE TABLE "CLIENTE" (
+    "IdCliente" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "nif" INTEGER UNIQUE,
+    "nombre" TEXT,
+    "apellido1" TEXT,
+    "apellido2" TEXT,
+    "domicilio" TEXT,
+    "cp" TEXT,
+    "ciudad" TEXT,
+    "telefono1" INTEGER,
+    "telefono2" INTEGER,
+    "email" TEXT,
+    "observaciones" TEXT
+);
+CREATE TABLE "compra" (
+    "IdCompra" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "fecha" INTEGER,
+    "IdProveedor" TEXT,
+      FOREIGN KEY ("IdProveedor") REFERENCES proveedor("IdProveedor") ON UPDATE  CASCADE ON DELETE SET NULL
+);
+CREATE TABLE "UNIDAD" (
+    "IdUnidad" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "IdVideojuego" INTEGER NOT NULL,
+    "IdCompra" INTEGER NOT NULL,
+    "preciocoste" FLOAT,
+      FOREIGN KEY ("IdVideojuego") REFERENCES videojuego("IdVideojuego") ON UPDATE  CASCADE ON DELETE SET NULL,
+      FOREIGN KEY ("IdCompra") REFERENCES compra("IdCompra") ON UPDATE  CASCADE ON DELETE SET NULL
+);
+CREATE TABLE "alquila" (
+    "IdAlquiler" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "fecha_salida" INTEGER NOT NULL,
+    "fecha_devolucion" INTEGER ,
+     "IdUnidad" INTEGER NOT NULL,
+    "IdCliente" INTEGER NOT NULL,
+      FOREIGN KEY ("IdUnidad") REFERENCES unidad("IdUnidad") ON UPDATE  CASCADE ON DELETE SET NULL,
+      FOREIGN KEY ("IdCliente") REFERENCES cliente("IdCliente") ON UPDATE  CASCADE ON DELETE SET NULL
+);

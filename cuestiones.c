49eb300a77e2ba4ad8b4c4bb970bfe153f76eb2b
Bartolomé Vich Lozano <tolo@tovilo.es>
2018-07-23 16:05:36 +0200

    Update cuestiones.c

--- cuestiones.c
+++ cuestiones.c
@@ -1,1 +1,23 @@
+/*
+* AUTOR: BarTOLOme Vich Lozano
+* EMAIL: amadews23@hotmail.com
+* FECHA: Julio del 2018
+*
+* Programa para un alquiler de videojuegos
+* 	Usa Glib y Sqlite
+*	Es en modo texto
+*
+* Implementacion de los prototipos para el manejo de cadenas
+* Archivo: cuestiones.c
+*
+*/
+#include <string.h>
+#include "cuestiones.h"
 
+void LimpiarTexto(gchar *cadena) {
+	gchar *p;
+	p = strchr (cadena, '\n');
+	if (p) {
+		*p = '\0';
+	}
+}

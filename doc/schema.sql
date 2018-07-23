9e741caa7d6e6de807d68167bd146123164f8dd5
Bartolomé Vich Lozano <tolo@tovilo.es>
2018-07-23 15:49:59 +0200

    Update and rename rent.sql to schema.sql

--- doc/schema.sql
+++ doc/schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE "GENERO" (
     "IdGenero" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     "nombre" TEXT
 );
+
 CREATE TABLE VIDEOJUEGO(
  IdVideojuego    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
   nombre  TEXT,
@@ -10,34 +11,12 @@ CREATE TABLE VIDEOJUEGO(
   precioalquiler FLOAT,
   FOREIGN KEY (IdGenero) REFERENCES genero(IdGenero) ON UPDATE  CASCADE ON DELETE SET NULL
 );
-CREATE TABLE PROVEEDOR (
-    "IdProveedor" INTEGER NOT NULL,
-    "nombre" TEXT,
-    "domicilio" TEXT,
-    "telefono1" INTEGER,
-    "telefono2" INTEGER,
-    "email" TEXT,
-    "observaciones" TEXT
-);
-CREATE TABLE "CLIENTE" (
-    "IdCliente" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "nif" INTEGER UNIQUE,
-    "nombre" TEXT,
-    "apellido1" TEXT,
-    "apellido2" TEXT,
-    "domicilio" TEXT,
-    "cp" TEXT,
-    "ciudad" TEXT,
-    "telefono1" INTEGER,
-    "telefono2" INTEGER,
-    "email" TEXT,
-    "observaciones" TEXT
-);
-CREATE TABLE "compra" (
+CREATE TABLE "COMPRA" (
     "IdCompra" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     "fecha" INTEGER,
     "IdProveedor" TEXT,
       FOREIGN KEY ("IdProveedor") REFERENCES proveedor("IdProveedor") ON UPDATE  CASCADE ON DELETE SET NULL
+
 );
 CREATE TABLE "UNIDAD" (
     "IdUnidad" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -50,14 +29,41 @@ CREATE TABLE "UNIDAD" (
 CREATE TABLE "ALQUILA" (
     "IdAlquiler" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     "fecha_salida" INTEGER NOT NULL,
-    "fecha_adevolver" INTEGER NOT NULL,
+     "fecha_adevolver" INTEGER NOT NULL,
     "fecha_devolucion" INTEGER DEFAULT NULL,
     "importe_pagado" FLOAT, 
     "importe_debido" FLOAT,
     "pagado" INTEGER DEFAULT NULL,
-    "IdUnidad" INTEGER NOT NULL,
+     "IdUnidad" INTEGER NOT NULL,
     "IdCliente" INTEGER NOT NULL,
       FOREIGN KEY ("IdUnidad") REFERENCES unidad("IdUnidad") ON UPDATE  CASCADE ON DELETE SET NULL,
       FOREIGN KEY ("IdCliente") REFERENCES cliente("IdCliente") ON UPDATE  CASCADE ON DELETE SET NULL
-)
-
+);
+CREATE TABLE "CLIENTE" (
+    "IdCliente" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "cnif" TEXT UNIQUE,
+    "nombre1" TEXT,
+    "nombre2" TEXT,
+    "domicilio" TEXT,
+    "cp" TEXT,
+    "ciudad" TEXT,
+    "telefono1" TEXT,
+    "telefono2" TEXT,
+    "email" TEXT,
+    "observaciones" TEXT
+);
+;
+CREATE TABLE "PROVEEDOR" (
+    "IdProveedor" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "cnif" TEXT UNIQUE,
+    "nombre1" TEXT,
+    "nombre2" TEXT,
+    "domicilio" TEXT,
+    "cp" TEXT,
+    "ciudad" TEXT,
+    "telefono1" TEXT,
+    "telefono2" TEXT,
+    "email" TEXT,
+    "observaciones" TEXT
+);
+;

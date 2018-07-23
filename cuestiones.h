95ce43cb179fd0c7680e2dcbacba8cf59da0f108
Bartolomé Vich Lozano <tolo@tovilo.es>
2018-07-23 16:03:25 +0200

    Update cuestiones.h

--- cuestiones.h
+++ cuestiones.h
@@ -1,1 +1,18 @@
+/*
+* AUTOR: BarTOLOme Vich Lozano
+* EMAIL: amadews23@hotmail.com
+* FECHA: Julio del 2018
+*
+* Programa que destinado para un alquiler de videojuegos
+* 	Usa Glib y Sqlite
+*	Es en modo texto
+*
+* Prototipos y tipos de datos para el manejo de cadenas
+* Archivo: cuestiones.h
+*
+*/
+#pragma once
+#include <glib.h>
 
+/*Eliminamos los /n del fgets*/
+void LimpiarTexto(gchar *cadena);

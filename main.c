4a7850b1f2d07b001a2dbed9128d366cc2817e74
Adaggio <adaggio@termodifusor4.(none)>
2018-08-15 14:40:17 +0200

    actualizando main.c

--- main.c
+++ main.c
@@ -18,40 +18,94 @@
 
 int main() {
 
-	gchar *tecla; //tecla sera un string para evitar que al insertar demasiados caracteres se copie en el siguiente fgets
+	//TODO Hacer usar argp.h y crear las funciones para recoger argumentos (Por Ejemplo cargar otra Bd) y 
+	//mostrar --help ... 
+	gchar *tecla; //tecla sera un string para evitar que al insertar demasiados caracteres desborde
 	gchar *TipoDePersona;
+
 	TipoDePersona = g_malloc(10);
+
+	tipo_vectorpersona vectorpersona;
+
+	for (int a=0; a < 12 ; a++) {
+		vectorpersona[a] = FALSE;
+
+
+	}
+	//vectorpersona[0] = TRUE;
+	//vectorpersona[1] = TRUE;
+	vectorpersona[1] = TRUE;
+	//vectorpersona[2] = TRUE;
+	vectorpersona[4] = TRUE;
+	//vectorpersona[6] = TRUE;
+
+
 	tecla = g_malloc(3);
-  
-	if (ConectarBd("rent.db") == FALSE) {
+	//tmp = g_malloc(3);
+	if (ConectarBd(BASEDATOS) == FALSE) {
 		return -1;
 	}
 	do {
 		//g_printf("\033c");
 		g_printf("SELECCIONE UNA OPCION\n");
 		g_printf("1-Para Insertar un Cliente\n");
-		g_printf("2-Para Insertar un Proveedor\n");
-		g_printf("5-Para SALIR\n");
+		g_printf("2-Para Ver Clientes\n");
+		//g_printf("3-Para Borrar un Cliente\n");
 
+		g_printf("4-Para Insertar un Proveedor\n");
+		g_printf("5-Para Ver Proveedores\n");
+		//g_printf("6-Para Borrar un Proveedor\n");
+
+		g_printf("7-Para SALIR\n");
+
+		//fgets (&tecla, 3, stdin);
 		fgets(tecla, 4, stdin);
+		//fgets(TextoReducido(1, tecla), 3, stdin);
+	//	fflush(stdin);
+
 		LimpiarTexto(tecla);
 
-		if ((g_strcmp0 (tecla, "1")) == 0) {
+		//g_stpcpy(TipoDePersona, TextoReducido(1, tecla));
+		//ReducirTexto0(1, tecla);
+		//LimpiarTexto(tecla);
+		//g_printf("TECLA:%c",TextoReducido(tecla));
+		//g_printf("Es la tecla %s\n",tecla);
+//		g_stpcpy (tmp,TextoReducido(tecla));
+				//g_stpcpy (tmp,tecla);
+		//tmp = TextoReducido(tecla);
+		//g_strndup (tecla, 2);
+//g_printf("Es la tecla %s\n",tecla);
+//g_printf("Es la tecla %s\n",tecla);
 
+//g_printf("el tamanyo del string es %d\n", (guint)strlen(tecla));
+		if ((g_strcmp0 (tecla, "1")) == 0) {
+		//if (tecla == '1') {
+			//g_printf("\033c");
 			g_stpcpy(TipoDePersona, "CLIENTE");
 			CrearPersona(TipoDePersona);
 		}
 		if ((g_strcmp0 (tecla, "2")) == 0) {
-
+		//if (tecla == '1') {
+			//g_printf("\033c");
+			g_stpcpy(TipoDePersona, "CLIENTE");
+			MostrarPersonas(TipoDePersona, vectorpersona);
+		}
+		if ((g_strcmp0 (tecla, "4")) == 0) {
+ 		//if (tecla == '2') {
+ 			//g_printf("\033c");
 			g_stpcpy(TipoDePersona, "PROVEEDOR");
 			CrearPersona(TipoDePersona);
 		}
+		if ((g_strcmp0 (tecla, "5")) == 0) {
+		//if (tecla == '1') {
+			//g_printf("\033c");
+			g_stpcpy(TipoDePersona, "PROVEEDOR");
+			MostrarPersonas(TipoDePersona, vectorpersona);
+		}
 
-	} while ( g_strcmp0 (tecla, "5") != 0 );
-	
-	/*Liberamos variables dinamicas*/
-	g_free(TipoDePersona);
-	g_free(tecla);
-	g_printf("Adews...\n");
+	} while ( g_strcmp0 (tecla, "7") != 0 );
+		/*Liberamos variables dinamicas*/
+		g_free(TipoDePersona);
+		g_free(tecla);
+		g_printf("Adeus...\n");
 }
-
